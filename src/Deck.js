14a69dd2578dc0a4204946f1f4da350914375e36
Chris Wirth <chris@goozmo.com>
2017-06-26 11:58:36 -0600

    swipe logging

--- src/Deck.js
+++ src/Deck.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 import {
   View,
   Animated,
-  PanResponder } from 'react-native';
+  PanResponder,
+  Dimensions } from 'react-native';
+
+  const SCREEN_WIDTH = Dimensions.get('window').width;
+  const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 
 class Deck extends Component {
   constructor(props) {
@@ -13,26 +17,60 @@ class Deck extends Component {
       onPanResponderMove: (event, gesture) => {
         position.setValue({ x: gesture.dx, y: gesture.dy });
       },
-      onPanResponderRlease: () => {}
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          console.log('swipe right');
+        } else if (gesture.dx < SWIPE_THRESHOLD) {
+          console.log('swipe left');
+        } else {
+          this.resetPosition();
+        }
+      }
     });
 
     this.state = { panResponder, position };
   }
 
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+      outputRange: ['-120deg', '0deg', '120deg']
+    });
+
+    return {
+      ...position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
 renderCards() {
-  return this.props.data.map(item => {
+  return this.props.data.map((item, index) => {
+    if (index === 0) {
+      return (
+        <Animated.View
+          key={item.id}
+          style={this.getCardStyle()}
+          {...this.state.panResponder.panHandlers}
+        >
+          {this.props.renderCard(item)}
+        </Animated.View>
+      );
+    }
     return this.props.renderCard(item);
   });
 }
 
   render() {
     return (
-      <Animated.View
-        style={this.state.position.getLayout()}
-        {...this.state.panResponder.panHandlers}
-      >
+      <View>
         {this.renderCards()}
-      </Animated.View>
+      </View>
+
     );
   }
 }
