370acb0d51d708d1b55ad442bc15bc7ff7650777
Chris Wirth <chriswirth@Chriss-MacBook-Pro.local>
2017-06-26 23:19:01 -0600

    swipe removing card and applying animation to the next one

--- src/Deck.js
+++ src/Deck.js
@@ -7,10 +7,17 @@ import {
 
   const SCREEN_WIDTH = Dimensions.get('window').width;
   const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+  const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  }
+
   constructor(props) {
     super(props);
+
     const position = new Animated.ValueXY();
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
@@ -19,18 +26,33 @@ class Deck extends Component {
       },
       onPanResponderRelease: (event, gesture) => {
         if (gesture.dx > SWIPE_THRESHOLD) {
-          console.log('swipe right');
-        } else if (gesture.dx < SWIPE_THRESHOLD) {
-          console.log('swipe left');
+          this.forceSwipe('right');
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+          this.forceSwipe('left');
         } else {
           this.resetPosition();
         }
       }
     });
 
-    this.state = { panResponder, position };
+    this.state = { panResponder, position, index: 0 };
   }
 
+  forceSwipe(direction) {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+    Animated.timing(this.state.position, {
+      toValue: { x, y: 0 },
+      duration: SWIPE_OUT_DURATION
+    }).start(() => this.onSwipeComplete(direction));
+  }
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
+  }
   getCardStyle() {
     const { position } = this.state;
     const rotate = position.x.interpolate({
@@ -43,14 +65,17 @@ class Deck extends Component {
       transform: [{ rotate }]
     };
   }
+
   resetPosition() {
     Animated.spring(this.state.position, {
       toValue: { x: 0, y: 0 }
-    }).start();
+    }).start(() => this.onSwipeComplete());
   }
+
 renderCards() {
-  return this.props.data.map((item, index) => {
-    if (index === 0) {
+  return this.props.data.map((item, i) => {
+    if (i < this.state.index) { return null; }
+    if (i === this.state.index) {
       return (
         <Animated.View
           key={item.id}
